c2a6b8f65249ff192aa440275d00e6b6832f19ef
Lucas Marinho <ls.marinho36@gmail.com>
2026-04-15 15:24:43 -0300

    adicionando comentários

--- src/main/java/dio/portifolio/config/SecurityConfig.java
+++ src/main/java/dio/portifolio/config/SecurityConfig.java
@@ -17,21 +17,25 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtFilter;
-
+    //Pipeline de segurança
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                //desativa csrf : proteção contra ataque de formulário
                 .csrf(csrf -> csrf.disable())
+                //permite abrir o h2 no navegador
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+                //define quem pode acessar o que
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/h2-console/**","/swagger-ui/**","/v3/api-docs/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/funcionarios").permitAll()
-
-                        .requestMatchers("/pontos/**").authenticated()         // 🔥 ADICIONA ISSO
-                        .requestMatchers("/banco-horas/**").authenticated()    // 🔥 E ISSO
-
+                        .requestMatchers(HttpMethod.POST, "/cadastro").permitAll()
+                        //rota autenticada
+                        .requestMatchers("/pontos/**").authenticated()
+                        .requestMatchers("/banco-horas/**").authenticated()
+                        //rota com acesso limitado ao admin
                         .requestMatchers("/admin/**").hasRole("ADMIN")
+                        //o restante precisa estar logado
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
@@ -40,3 +44,7 @@ public class SecurityConfig {
     }
 
 }
+// Este config
+// 1. Verifica quem pode acessar cada rota
+// 2. Quando precisa de login
+// 3. Quando precisa ser admin
